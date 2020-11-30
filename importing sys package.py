fd1bbafe0ad494f9b3b751c1192f47b94cb2c4bc
SANDHIYA <sandhiya22062001@gmail.com>
2020-11-30 19:26:02 +0530

    DAY 7 task

---
+++ importing sys package.py
@@ -0,0 +1,6 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import sys
+>>> sys.path
+['', 'C:\\Users\\sandhiya\\AppData\\Local\\Programs\\Python\\Python38\\Lib\\idlelib', 'C:\\Users\\sandhiya\\AppData\\Local\\Programs\\Python\\Python38\\python38.zip', 'C:\\Users\\sandhiya\\AppData\\Local\\Programs\\Python\\Python38\\DLLs', 'C:\\Users\\sandhiya\\AppData\\Local\\Programs\\Python\\Python38\\lib', 'C:\\Users\\sandhiya\\AppData\\Local\\Programs\\Python\\Python38', 'C:\\Users\\sandhiya\\AppData\\Local\\Programs\\Python\\Python38\\lib\\site-packages']
+>>> 
---
+++ randomnumber.py
@@ -0,0 +1,7 @@
+import random as r
+a=r.randint(1,100)
+print(a)
+b=r.randint(1,100)
+print(b)
+c=r.randint(1,100)
+print(c)
