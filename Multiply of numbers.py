17673cbd87f035a3dcfa68545c2afbbea37afa2c
SANDHIYA <sandhiya22062001@gmail.com>
2020-12-01 17:38:22 +0530

    DAY 9 Task

---
+++ Multiply of numbers.py
@@ -0,0 +1,5 @@
+number= [5,2,4,6,7,8]
+a=5
+print("THE GIVEN NUMBERS ARE:",number)
+number_multiply=list(map(lambda b:b*a,number))
+print("THE NUMBER AFTER MULTIPLY BY 5 IS",number_multiply)
---
+++ evennumbers.py
@@ -0,0 +1,5 @@
+numbers= [2,3,4,5,6,7,8,9,10]
+print("THE EVEN NUMBERS ARE:")
+for i in numbers:
+    if i%2==0:
+        print(i)
